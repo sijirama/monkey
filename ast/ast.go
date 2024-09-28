18e7d741d3192262a4da6b012fdfefcf9b4efb44
sijirama <gbemilesanmi@gmail.com>
2024-09-28 23:13:56 +0100

    ast and parser work

---
+++ ast/ast.go
@@ -0,0 +1,45 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type Identifier struct {
+	Token token.Token //NOTE the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type LetStatement struct { // let x = 4;
+	Token token.Token //the token.LET token
+	Name  *Identifier //Name to hold the identifier of the binding
+	Value Expression  //Value for the expression that produces the value
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
