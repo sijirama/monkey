6a189643d4fe02251aa5bb820791d5ff74a5f285
sijirama <gbemilesanmi@gmail.com>
2024-12-15 10:38:42 +0100

    infix pratt parsing

--- parser/parser.go
+++ parser/parser.go
@@ -162,7 +162,9 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 }
 
 func (p *Parser) parseExpression(precedence int) ast.Expression {
+
 	prefix := p.prefixParseFns[p.curToken.Type]
+
 	if prefix == nil {
 		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
