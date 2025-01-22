89bf394c9779ceed23cea7ea828ee1bab3b5d141
sijirama <gbemilesanmi@gmail.com>
2025-01-22 22:29:53 +0100

    checkpoint: fucntion calls work and their local bindings work

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -125,6 +125,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 
 	case *ast.FunctionLiteral:
 		c.enterScope()
+
+		for _, p := range node.Parameters {
+			c.symbolTable.Define(p.Value)
+		}
+
 		err := c.Compile(node.Body)
 		if err != nil {
 			return err
@@ -137,7 +142,7 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 		numLocals := c.symbolTable.numDefinitions
 		instructions := c.leaveScope()
-		compiledFn := &object.CompiledFunction{Instructions: instructions, NumLocals: numLocals}
+		compiledFn := &object.CompiledFunction{Instructions: instructions, NumLocals: numLocals, NumParameters: len(node.Parameters)}
 		c.emit(code.OpConstant, c.addConstant(compiledFn))
 	case *ast.ReturnStatement:
 		err := c.Compile(node.ReturnValue)
